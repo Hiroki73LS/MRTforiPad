58d69dcde7586541ac7e609e8fa1d512ff188c09
Hiroki mac air <aichan_lovepower@yahoo.co.jp>
2022-10-21 21:41:05 +0900

    Initial Commit

---
+++ Shared/MultiRecTimerforiPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiRecTimerforiPadApp.swift
+//  Shared
+//
+//  Created by 林宏樹 on 2022/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MultiRecTimerforiPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
